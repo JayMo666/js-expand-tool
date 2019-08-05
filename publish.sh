8a2936e53e83d6ac66470586f6b1e30ffdcaea08
莫爵贵 <mojuegui@dev.hdvon.com>
2019-08-05 18:33:53 +0800

    [build] 0.3.37

--- publish.sh
+++ publish.sh
@@ -2,18 +2,12 @@
 set -e
 echo "Enter release version:"
 read VERSION
-read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-echo # (optional) move to a new line
-if true
-then
-  echo "Releasing $VERSION ..."
-
-  #commit
-  git add -A
-  git commit -m "[build] $VERSION"
-  # npm version $VERSION --meassage "[release] $VERSION"
-  git push origin master
-  echo 'success push to github!'
-  #publish
-  npm publish
-fi
+echo "Releasing $VERSION ..."
+#commit
+git add -A
+git commit -m "[build] $VERSION"
+# npm version $VERSION --meassage "[release] $VERSION"
+git push origin master
+echo 'success push to github!'
+#publish
+npm publish
